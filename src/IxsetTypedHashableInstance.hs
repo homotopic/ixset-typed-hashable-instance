ba461fc4508ce68fdbd0786f1e5ec78d8f2902d7
Dan Firth <locallycompact@gmail.com>
2020-07-27 15:03:51 +0100

    Create the project

---
+++ src/IxsetTypedHashableInstance.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Daniel Firth
+SPDX-License-Identifier: MIT
+Maintainer: Daniel Firth <dan.firth@homotopic.tech>
+
+Hashable orphan instance for ixset-typed.
+-}
+
+module IxsetTypedHashableInstance
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
