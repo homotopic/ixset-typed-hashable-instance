1a5ad7316c6a5941b739d520e314260e19c923fb
Dan Firth <locallycompact@gmail.com>
2020-07-27 16:16:58 +0100

    fix

--- src/Data/IxSet/Typed/Hashable.hs
+++ src/Data/IxSet/Typed/Hashable.hs
@@ -1,7 +1,7 @@
 module Data.IxSet.Typed.Hashable where
 
 import Data.IxSet.Typed
-import Hashable
+import Data.Hashable
 
 instance (Indexable ixs x, Hashable x) => Hashable (IxSet ixs x) where
   hashWithSalt n x = n `hashWithSalt` toList x
